f9782a4f25585302068b47654899cf2308ed6f24
doganuraydeniz <doganur.aydeniz@sekompos.com>
2025-01-11 13:22:11 +0300

    Add app icon and splash screen

--- app/src/main/java/com/doganur/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/doganur/movieapp/presentation/MainActivity.kt
@@ -4,27 +4,27 @@ import BottomNavigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.navigation.compose.rememberNavController
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
 import com.doganur.movieapp.navigation.NavigationGraph
 import com.doganur.movieapp.navigation.Screen
-import dagger.hilt.android.AndroidEntryPoint
 import com.doganur.movieapp.presentation.theme.AppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             AppTheme {
                 val navController = rememberNavController()
                 val startDestination = Screen.Home
-                
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     bottomBar = {
